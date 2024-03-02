734a2fed34a34f7df05b379680091095b6d2e52d
harrykhlo <harrykhlo@gmail.com>
2024-03-02 15:42:30 +1300

    Add DELETE user end point

--- UserApi/UserApi/Controllers/UserApiController.cs
+++ UserApi/UserApi/Controllers/UserApiController.cs
@@ -8,7 +8,7 @@ namespace UserApi.Controllers
     //[Route("api/[controller]")]
     [Route("api/users")]
     [ApiController]
-    public class UserApiController:ControllerBase
+    public class UserApiController : ControllerBase
     {
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)] //or use [ProducesResponseType(200)]
@@ -23,9 +23,9 @@ namespace UserApi.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)] //or use [ProducesResponseType(404)]
         public ActionResult<UserDto> GetUser(int id)
         {
-            if (id == 0) 
-            { 
-                return BadRequest(); 
+            if (id == 0)
+            {
+                return BadRequest();
             };
             var user = UserStore.userList.FirstOrDefault(u => u.Id == id);
             if (user == null)
@@ -36,11 +36,11 @@ namespace UserApi.Controllers
         }
 
         [HttpPost]
-        [ProducesResponseType(StatusCodes.Status201Created)] 
-        [ProducesResponseType(StatusCodes.Status400BadRequest)] 
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public ActionResult<UserDto> CreateUser([FromBody] UserDto userDto) 
-        { 
+        public ActionResult<UserDto> CreateUser([FromBody] UserDto userDto)
+        {
             if (userDto == null)
             {
                 return BadRequest(userDto);
@@ -49,16 +49,35 @@ namespace UserApi.Controllers
             {
                 return BadRequest(userDto);
             }
+            //TODO: validate if email is format correct or not
             //User's email have to be unique
-            if (UserStore.userList.FirstOrDefault(u=>u.Email.ToLower()== userDto.Email.ToLower())!=null)
+            if (UserStore.userList.FirstOrDefault(u => u.Email.ToLower() == userDto.Email.ToLower()) != null)
             {
-                //TODO: check why ModelState does not work
                 ModelState.AddModelError("UserEmailValidationError", "User's email already exists");
                 return BadRequest(ModelState);
             }
             userDto.Id = UserStore.userList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
             UserStore.userList.Add(userDto);
-            return CreatedAtRoute("GetTheUser", new { id = userDto.Id}, userDto);
+            return CreatedAtRoute("GetTheUser", new { id = userDto.Id }, userDto);
+        }
+
+        [HttpDelete("{id:int}", Name = "DeleteUser")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult DeleteUser(int id) 
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var user = UserStore.userList.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            UserStore.userList.Remove(user);
+            return NoContent();
         }
     }
 }
